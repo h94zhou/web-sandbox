7d54ec3fb7fdbeed51fc4943bdd7c5be4a93951a
h94zhou <h94zhou@uwaterloo.ca>
2020-10-30 14:48:08 -0400

    Add rechart to substation view

--- src/pages/Substation.js
+++ src/pages/Substation.js
@@ -4,15 +4,22 @@ import {
   Link
     } from 'react-router-dom';
 
+import { StationGraph } from './../components/StationGraph';
+
 function Substation() {
     let { substation } = useParams();
     const url = "/" + substation + "/asset1";
     return (
-        <div>
-            <h2>Welcome to { substation }</h2>
-            <Link to={ url } >Asset 1 </Link>
-
-
+        <div style={{margin:"10px 10px 10px 10px"}}>
+            <div style={{margin:"0 0 30px 0"}}>
+                <h2>Welcome to { substation }</h2>
+                <Link to={ url } >Asset 1 </Link>
+    
+            </div>
+            <div>
+                <h4>Graph</h4>
+                <StationGraph />
+            </div>
         </div>
     );
 }
